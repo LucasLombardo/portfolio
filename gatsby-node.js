f02df936648aa9be05caba5c1e75baff04dd3238
Lucas Lombardo <Lucasl@bu.edu>
2019-01-12 11:16:41 -0500

    remove createPages for pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,80 +9,80 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return new Promise((resolve, reject) => {
     // ==== PAGES (WORDPRESS NATIVE) ====
+    // graphql(
+    //   `
+    //     {
+    //       allWordpressPage {
+    //         edges {
+    //           node {
+    //             id
+    //             slug
+    //             status
+    //             template
+    //           }
+    //         }
+    //       }
+    //     }
+    //   `
+    // )
+    //   .then(result => {
+    //     if (result.errors) {
+    //       console.log(result.errors)
+    //       reject(result.errors)
+    //     }
+
+    //     // Create Page pages.
+    //     const pageTemplate = path.resolve(`./src/templates/page.js`)
+    //     _.each(result.data.allWordpressPage.edges, edge => {
+    //       // ignore home page, it's content will be pulled into index.js
+    //       if (edge.node.slug !== 'home') {
+    //         createPage({
+    //           path: `/${edge.node.slug}/`,
+    //           // uses page template to construct the page
+    //           component: slash(pageTemplate),
+    //           // context gives the page the ability to do page queries
+    //           context: {
+    //             id: edge.node.id,
+    //           },
+    //         })
+    //       }
+    //     })
+    //   })
+    // ==== POSTS (WORDPRESS NATIVE AND ACF) ====
+    //   .then(() => {
     graphql(
       `
         {
-          allWordpressPage {
+          allWordpressPost {
             edges {
               node {
                 id
                 slug
                 status
                 template
+                format
               }
             }
           }
         }
       `
-    )
-      .then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
-
-        // Create Page pages.
-        const pageTemplate = path.resolve(`./src/templates/page.js`)
-        _.each(result.data.allWordpressPage.edges, edge => {
-          // ignore home page, it's content will be pulled into index.js
-          if (edge.node.slug !== 'home') {
-            createPage({
-              path: `/${edge.node.slug}/`,
-              // uses page template to construct the page
-              component: slash(pageTemplate),
-              // context gives the page the ability to do page queries
-              context: {
-                id: edge.node.id,
-              },
-            })
-          }
+    ).then(result => {
+      if (result.errors) {
+        console.log(result.errors)
+        reject(result.errors)
+      }
+      const postTemplate = path.resolve(`./src/templates/post.js`)
+      _.each(result.data.allWordpressPost.edges, edge => {
+        createPage({
+          path: edge.node.slug,
+          component: slash(postTemplate),
+          context: {
+            id: edge.node.id,
+          },
         })
       })
-      // ==== POSTS (WORDPRESS NATIVE AND ACF) ====
-      .then(() => {
-        graphql(
-          `
-            {
-              allWordpressPost {
-                edges {
-                  node {
-                    id
-                    slug
-                    status
-                    template
-                    format
-                  }
-                }
-              }
-            }
-          `
-        ).then(result => {
-          if (result.errors) {
-            console.log(result.errors)
-            reject(result.errors)
-          }
-          const postTemplate = path.resolve(`./src/templates/post.js`)
-          _.each(result.data.allWordpressPost.edges, edge => {
-            createPage({
-              path: edge.node.slug,
-              component: slash(postTemplate),
-              context: {
-                id: edge.node.id,
-              },
-            })
-          })
-          resolve()
-        })
-      })
+      resolve()
+    })
+    //   })
   })
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,37 +1,15 @@
 import React from 'react'
-import { StaticQuery, Link, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 import { ScNav } from '../styles'
 
 const Nav = () => (
-  <StaticQuery
-    query={NAV_QUERY}
-    render={data => (
-      <ScNav>
-        <Link to="">Lucas Lombardo</Link>
-        {data.allWordpressPage.edges.map(
-          ({ node }) =>
-            node.slug !== 'home' && (
-              <Link to={node.slug} key={node.id}>
-                {node.title}
-              </Link>
-            )
-        )}
-      </ScNav>
-    )}
-  />
+  <ScNav>
+    <Link to="">Lucas Lombardo</Link>
+    <a href="#about">About</a>
+    <a href="#about">Work</a>
+    <Link to="blog">Blog</Link>
+    <a href="#about">Contact</a>
+  </ScNav>
 )
-const NAV_QUERY = graphql`
-  query NAV_QUERY {
-    allWordpressPage(sort: { fields: date, order: ASC }) {
-      edges {
-        node {
-          id
-          slug
-          title
-        }
-      }
-    }
-  }
-`
 
 export default Nav
