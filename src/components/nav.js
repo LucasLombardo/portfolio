2a820bff0fb4adf96a84b6173583ff7595085d8b
Lucas Lombardo <Lucasl@bu.edu>
2019-01-14 13:14:32 -0500

    adds scrollto on nav anchors

--- src/components/nav.js
+++ src/components/nav.js
@@ -32,17 +32,38 @@ export default class nav extends Component {
   toggleNav = () => {
     this.setState({ isOpen: !this.state.isOpen })
   }
+
+  scrollToID = id => {
+    const currPos = window.pageYOffset
+    const top = id ? document.querySelector(id).offsetTop : 0
+    // if distance to scroll over 2400px, jump there rather than scrolling
+    const behavior = Math.abs(top - currPos) > 2400 ? 'auto' : 'smooth'
+    window.scrollTo({ top, behavior })
+  }
+
   render() {
     const { isHome } = this.props
     const { fixed } = this.state
     const navClasses = isHome ? (fixed ? 'home fixed' : 'home') : ''
     return (
       <ScNav className={navClasses}>
-        <Link to="/">Lucas Lombardo</Link>
-        <Link to="/#About">About</Link>
-        <Link to="/#Work">Work</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/#Contact">Contact</Link>
+        {isHome ? (
+          <>
+            <a onClick={() => this.scrollToID('')}>Lucas Lombardo</a>
+            <a onClick={() => this.scrollToID('#About')}>About</a>
+            <a onClick={() => this.scrollToID('#Work')}>Work</a>
+            <Link to="/blog">Blog</Link>
+            <a onClick={() => this.scrollToID('#Contact')}>Contact</a>
+          </>
+        ) : (
+          <>
+            <Link to="/">Lucas Lombardo</Link>
+            <Link to="/#About">About</Link>
+            <Link to="/#Work">Work</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/#Contact">Contact</Link>
+          </>
+        )}
       </ScNav>
     )
   }
