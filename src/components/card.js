ea8ec153975036056a1afd4ac169067a87b54f66
Lucas Lombardo <Lucasl@bu.edu>
2019-01-14 20:52:01 -0500

    add animations on scroll

--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { ScCard } from '../styles/'
+import ScrollAnimation from 'react-animate-on-scroll'
 
 const Card = ({ src, title, content, thumbnailLeft, children }) => (
-  <ScCard className={thumbnailLeft ? 'left' : 'right'}>
-    <div className="image-wrapper">
-      <img src={src} alt={title} />
-    </div>
-    <div className="content">
-      {title && <h3>{title}</h3>}
-      {content && <div dangerouslySetInnerHTML={{ __html: content }} />}
-      <div className="card-footer">{children}</div>
-    </div>
-  </ScCard>
+  <ScrollAnimation animateIn="rotate-in" animateOnce={true}>
+    <ScCard className={thumbnailLeft ? 'left' : 'right'}>
+      <div className="image-wrapper">
+        <img src={src} alt={title} />
+      </div>
+      <div className="content">
+        {title && <h3>{title}</h3>}
+        {content && <div dangerouslySetInnerHTML={{ __html: content }} />}
+        <div className="card-footer">{children}</div>
+      </div>
+    </ScCard>
+  </ScrollAnimation>
 )
 
 export default Card
--- src/components/project.js
+++ src/components/project.js
@@ -1,37 +1,40 @@
 import React from 'react'
 import Button from './button'
+import ScrollAnimation from 'react-animate-on-scroll'
 
 const Project = ({
   info: { title, thumbnail, description, link, tags, code_link },
 }) => (
-  <div className="project">
-    <a href={link} target="_blank" rel="noopener noreferrer">
-      <img src={thumbnail} alt={title} />
-    </a>
-    <div className="project-info">
+  <ScrollAnimation animateIn="rotate-in" animateOnce={true}>
+    <div className="project">
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <h3>{title}</h3>
+        <img src={thumbnail} alt={title} />
       </a>
-      <p className="tags">
-        {tags.split(',').map(tag => (
-          <li key={tag}>
-            <span>{tag}</span>
-          </li>
-        ))}
-      </p>
-      <p className="description">{description}</p>
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <Button>Live Project</Button>
-      </a>
-      {code_link ? (
-        <a href={code_link} target="_blank" rel="noopener noreferrer">
-          <Button btnType="secondary">Project Code</Button>
+      <div className="project-info">
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <h3>{title}</h3>
         </a>
-      ) : (
-        <p className="no-code">Private Repo, code available on request</p>
-      )}
+        <p className="tags">
+          {tags.split(',').map(tag => (
+            <li key={tag}>
+              <span>{tag}</span>
+            </li>
+          ))}
+        </p>
+        <p className="description">{description}</p>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <Button>Live Project</Button>
+        </a>
+        {code_link ? (
+          <a href={code_link} target="_blank" rel="noopener noreferrer">
+            <Button btnType="secondary">Project Code</Button>
+          </a>
+        ) : (
+          <p className="no-code">Private Repo, code available on request</p>
+        )}
+      </div>
     </div>
-  </div>
+  </ScrollAnimation>
 )
 
 export default Project
